baed4cbeef48b89a7720cd90634e0a0218a0a7e9
Leo <leozagatto@gmail.com>
2019-10-28 19:09:38 -0300

    Adicionando quests e NPCs

---
+++ ProjetoArt3mis/Assets/Scripts/dialogHolder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogHolder : MonoBehaviour
+{
+
+    public string dialogue;
+    private DialogueManager dMan;
+
+    public string[] dialogLines;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dMan = FindObjectOfType<DialogueManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                //dMan.ShowBox(dialogue);
+                if (!dMan.dialogActive)
+                {
+                    dMan.dialogueLines = dialogLines;
+                    dMan.currentLine = 0;
+                    dMan.ShowDialogue();
+                }
+
+                if (transform.parent.GetComponent<VillagerMovement>() != null)
+                {
+                    transform.parent.GetComponent<VillagerMovement>().canMove = false;
+                }
+            }
+        }
+    }
+}
