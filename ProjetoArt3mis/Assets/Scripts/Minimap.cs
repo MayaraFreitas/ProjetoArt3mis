f98ba879fe2d718e673310886dddf52bf4b90a9e
MayaraFreitas <mah.alvesfreitas@gmail.com>
2020-02-07 17:18:19 -0300

    ajuste e imagens

--- ProjetoArt3mis/Assets/Scripts/Minimap.cs
+++ ProjetoArt3mis/Assets/Scripts/Minimap.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Minimap : MonoBehaviour
 {
     public Transform player;
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = transform.GetComponent<Camera>();
+        Matrix4x4 mat = camera.projectionMatrix;
+        mat *= Matrix4x4.Scale(new Vector3(-1, 1, 1));
+        camera.projectionMatrix = mat;
+    }
+
     private void LateUpdate()
     {
         Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
         newPosition.z = transform.position.z;
         transform.position = newPosition;
     }
